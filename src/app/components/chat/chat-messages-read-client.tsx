c6980784a4c446ea02991616b71cea65b1a4cc95
Yago <yarlorenzo@gmail.com>
2023-10-01 18:05:02 +0200

    Fix an infinite loop on db calls

--- src/app/components/chat/chat-messages-read-client.tsx
+++ src/app/components/chat/chat-messages-read-client.tsx
@@ -2,8 +2,6 @@ import {type ReactNode} from "react";
 import {createClientComponentClient} from "@supabase/auth-helpers-nextjs";
 import {useContext, useEffect, useState} from "react";
 import {SessionContext} from "@/app/providers";
-import {router} from "next/client";
-import {useRouter} from "next/navigation";
 
 type Filters = {
     room_id?: string
@@ -23,7 +21,6 @@ export function ChatMessagesRead({
     const database = createClientComponentClient();
     const {sessionContext: session}: any = useContext(SessionContext);
     const [unreadMessages, setUnreadMessages] = useState<any>([]) // or appropriate initial state
-    const router = useRouter()
 
     useEffect(() => {
         let query = database.from('chat_message_read')
@@ -54,7 +51,16 @@ export function ChatMessagesRead({
                     schema: 'public',
                     table: 'chat_message_read',
                 },
-                router.refresh
+                ({new: newData}: any) => {
+                    console.log('newData', newData)
+                    setUnreadMessages((prevUnreadMessages: any) => [...prevUnreadMessages || []]
+                        .map((msg: any) => msg.message_id === newData.message_id ? {...msg, ...newData} : msg)
+                        .filter((msg: any) => {
+                            if (filters?.room_id && msg.room_id !== filters?.room_id) return false
+                            if (filters?.user_id && msg.user_id !== filters?.user_id) return false
+                            return !(filters?.unreadOnly && msg.read_at !== null)
+                        }))
+                }
             )
             .subscribe()
 
