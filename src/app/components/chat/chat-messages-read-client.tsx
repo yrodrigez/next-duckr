6951df16a37347d1c54b7a711e4afdf007a48098
Yago <yarlorenzo@gmail.com>
2023-10-01 18:20:01 +0200

    Fixed an issue with infinite calls with channels

--- src/app/components/chat/chat-messages-read-client.tsx
+++ src/app/components/chat/chat-messages-read-client.tsx
@@ -51,7 +51,7 @@ export function ChatMessagesRead({
                     schema: 'public',
                     table: 'chat_message_read',
                 },
-                ({new: newData}: any) => {
+                (newData: any) => {
                     console.log('newData', newData)
                     setUnreadMessages((prevUnreadMessages: any) => [...prevUnreadMessages || []]
                         .map((msg: any) => msg.message_id === newData.message_id ? {...msg, ...newData} : msg)
