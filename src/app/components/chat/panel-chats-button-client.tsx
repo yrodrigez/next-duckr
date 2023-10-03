f624b309beb26e43d9a503462ef508c6ed78d665
Yago <yarlorenzo@gmail.com>
2023-10-03 19:04:06 +0200

    Change to get the unread messages from the context

--- src/app/components/chat/panel-chats-button-client.tsx
+++ src/app/components/chat/panel-chats-button-client.tsx
@@ -1,31 +1,28 @@
 'use client'
-
 import {IconMessages} from "@tabler/icons-react";
-import {useChatMessagesRead} from "@/app/components/chat/chat-messages-read-hook";
+import {type MessageReadEvent} from "@/app/components/chat/chat-messages-read-hook";
 import {useContext, useEffect, useState} from "react";
-import {SessionContext} from "@/app/providers";
+import {SessionContext, ChatMessagesReadContext} from "@/app/providers";
 
 export function PanelChatsButton() {
     const {sessionContext: session}: any = useContext(SessionContext);
-    const unreadMessages = useChatMessagesRead({
-        user_id: session?.user?.id,
-        unreadOnly: true
-    })
+    const unreadMessages = useContext(ChatMessagesReadContext);
 
     const [hasUnreadMessages, setHasUnreadMessages] = useState<number>(0)
 
     useEffect(() => {
-        setHasUnreadMessages(unreadMessages?.length || 0)
+        setHasUnreadMessages(unreadMessages?.filter((msg: MessageReadEvent) =>
+            msg.user_id === session?.user?.id && !msg.read_at
+        ).length || 0)
     }, [unreadMessages]);
 
     return (
         <div className="relative">
             <IconMessages size={40} color="white" className="m-0"/>
             {
-                (unreadMessages?.length || 0) > 0 && (
+                (hasUnreadMessages || 0) > 0 && (
                     <div
-                        className="absolute -top-1 -right-1 p-1 py-1 -z-10 rounded-full bg-sky-500 flex justify-center items-center">
-                        <p className="text-white text-xs"></p>
+                        className="absolute -top-2 -right-1 p-[0.35rem] -z-10 rounded-full bg-sky-500 animate-bounce-short animate-appearance-in">
                     </div>
                 )
             }
