ad9f1406a92687dc86baaa497d63e5f1b20be550
Yago <yarlorenzo@gmail.com>
2023-10-03 19:02:01 +0200

    Added the messages event to the context

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -5,23 +5,44 @@ import {NextUIProvider} from '@nextui-org/react'
 import {createClientComponentClient, type Session} from '@supabase/auth-helpers-nextjs'
 import {createContext, useState, useEffect} from 'react'
 import {redirect, usePathname} from "next/navigation";
+import {MessageReadEvent, useChatMessagesRead} from "@/app/components/chat/chat-messages-read-hook";
 
-const SessionContext = createContext({} as Session | null)
-export {SessionContext}
+export const SessionContext = createContext({} as Session | null)
+export const ChatMessagesReadContext = createContext<MessageReadEvent[]>([]);
 
-export function Providers({session, children}: { session: Session | null, children: React.ReactNode }) {
+export function Providers({
+                              session,
+                              children
+                          }: { session: Session | null, children: React.ReactNode }) {
     const [statedSession, setSession] = useState<Session | null>(session)
     const pathName = usePathname()
     const {user} = statedSession || {}
+    const chatMessagesRead = useChatMessagesRead()
 
-    if(!user && pathName !== '/login')
+    if (!user && pathName !== '/login') {
         redirect('/login')
-    else if (user && !user.user_metadata?.user_name && pathName !== '/login/update-user-name')
+    } else if (user && !user.user_metadata?.user_name && pathName !== '/login/update-user-name') {
         redirect('/login/update-user-name')
+    }
+
+    (() => {
+        const resizeManager = () => {
+            const h = window.innerHeight
+            document.body.style.height = `${h}px`
+        }
+        window.addEventListener('resize', resizeManager)
+        resizeManager()
+    })()
+
     return (
         <NextUIProvider>
-            <SessionContext.Provider value={{sessionContext: statedSession, setSession} as any}>
-                {children}
+            <SessionContext.Provider value={{
+                sessionContext: statedSession,
+                setSession
+            } as any}>
+                <ChatMessagesReadContext.Provider value={chatMessagesRead}>
+                    {children}
+                </ChatMessagesReadContext.Provider>
             </SessionContext.Provider>
         </NextUIProvider>
     )
