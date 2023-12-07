4f6d600bc2530f978c9bb82bc06fc0dac17f24cb
Yago <yarlorenzo@gmail.com>
2023-12-07 18:04:58 +0100

    Solved an issue that when selectin the replies these weren't showing

--- src/app/components/tab-view-client.tsx
+++ src/app/components/tab-view-client.tsx
@@ -5,21 +5,21 @@ import {type ReactNode} from "react";
 export function TabView({tabs}: {
     tabs: { label: string, icon?: any, name: string, children: ReactNode }[],
 }) {
-    const [activeTab, setActiveTab] = useState(tabs[0].name)
+    const [activeTab, setActiveTab] = useState(tabs[0])
 
     return (
         <div className="flex flex-col h-full overflow-auto">
             <div className="flex">
                 {tabs.map((tab, index) => (
                     <button key={index}
-                            className={`flex flex-col flex-1 items-center text-white text-sm font-bold py-2 px-3 border-b-2 border-transparent hover:border-white/30 focus:outline-none focus:border-white/30 transition-all ${activeTab === tab.name ? 'border-white/30' : ''}`}
-                            onClick={() => setActiveTab(tab.name)}>
+                            className={`flex flex-col flex-1 items-center text-white text-sm font-bold py-2 px-3 border-b-2 border-transparent hover:border-white/30 focus:outline-none focus:border-white/30 transition-all ${activeTab.name === tab.name ? 'border-white/30' : ''}`}
+                            onClick={() => setActiveTab(tab)}>
                         <span className="text-sm">{tab.label}</span>
                     </button>
                 ))}
             </div>
             <div className="overflow-auto">
-                {tabs.find(tab => tab.name === activeTab)?.children || []}
+                {activeTab.children || []}
             </div>
         </div>
     )
