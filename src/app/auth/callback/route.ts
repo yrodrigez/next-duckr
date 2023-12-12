78d7656a0af13de4468e58582a83965d2fbb9bc6
Yago <yarlorenzo@gmail.com>
2023-12-12 14:02:50 +0100

    Solved an issue with authentication on prod

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -1,6 +1,8 @@
 import {createRouteHandlerClient} from "@supabase/auth-helpers-nextjs";
 import {cookies, headers} from "next/headers";
 import {NextResponse, type NextRequest} from "next/server";
+const PRODUCTION = process.env.NODE_ENV === 'production'
+const PRODUCTION_ORIGIN = 'https://www.duckrapp.com/'
 
 export const dynamic = 'force-dynamic'
 
@@ -11,6 +13,10 @@ export async function GET(request: NextRequest) {
     if (code) {
         const database = createRouteHandlerClient({cookies})
         await database.auth.exchangeCodeForSession(code)
+    }
+
+    if (PRODUCTION) {
+        return NextResponse.redirect(PRODUCTION_ORIGIN + redirect)
     }
 
     return NextResponse.redirect(requestURL.origin + redirect)
